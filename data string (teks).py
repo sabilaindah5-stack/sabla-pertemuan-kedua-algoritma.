c8b75106f593ea85095d5946e35f60c0441fbe6e
sabilaindah5-stack <sabilaindah5@gmail.com>
2025-10-01 11:22:16 +0700

    Add files via upload

---
+++ data string (teks).py
@@ -0,0 +1,13 @@
+nama_depan = "sabila"
+nama_belakang = "bila"
+nama_lengkap = nama_depan + " " + nama_belakang
+usia = '18'
+alamat = 'pangkalpinang'
+kata_mutiara = " Don't judge a book by its cover "
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari',alamat, ', kata mutiara :',kata_mutiara)')
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(alamat))
+print('Tipe dari kata_mutiara:' type(kata_mutiara))
+
